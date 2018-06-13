88f3ca09967656ca0b1ff1e5a0c582b80e832b4f
imertz <imertz@protonmail.com>
2018-06-13 20:33:21 +0300

    Tested some new features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react"
 import ReactDOM from "react-dom";
 import moment from "moment"
+import Mouse from './Mouse'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.launchClock()
-        this.state = {clock: moment().format("LTS")}
+        this.state = {clock: moment().format("LTS"),
+        counter: 0
+        }
+        this.handleClick = this.handleClick.bind(this)
+
+    }
+
+    handleClick(e) {
+        this.setState({counter: ++this.state.counter})
     }
 
     launchClock() {
@@ -23,9 +32,17 @@ class App extends React.Component {
         return (
             <div>
                 <p>{this.state.clock}</p>
+                <Mouse 
+                    counter={this.state.counter}
+                    handler={this.handleClick}
+                />
             </div>
-        )}
+        )
     }
+}
+
+
+
 ReactDOM.render(<App />, document.getElementById("app"))
 
 export default App;
---
+++ src/Mouse.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+// class Mouse extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {counter: 0}
+//     }
+
+//     handleClick(event) {
+//         this.setState({counter: ++this.state.counter})
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <button
+//                 onClick={this.handleClick.bind(this)}
+//                 className="btn btn-primary">
+//                 Don't click me {this.state.counter} times!
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
+
+
+const Mouse = (props) => (
+    <button
+        onClick={props.handler}
+        className="btn btn-danger">
+        Increase Volume (Current Volume is {props.counter})
+    </button>
+);
+
+export default Mouse;
+
+
